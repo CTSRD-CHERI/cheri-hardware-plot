ced5d51d1a782ab126d8977211c88a98b21c34e6
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2019-08-22 14:32:28 +0100

    print average,geomean and high/low values for each configuration

--- drawplot/plot.py
+++ drawplot/plot.py
@@ -31,6 +31,8 @@
 
 import numpy as np
 import pandas as pd
+from scipy.stats.mstats import gmean
+import pprint
 import matplotlib.patches as mpatches
 from enum import Enum
 from drawplot.data import index_cat_data, std_norm, overheads2median
@@ -47,7 +49,7 @@ GraphType = Enum('GraphType', ['bar','box'])
 # internal graph draw function
 def draw (
         ax, families, confs, metrics, widths, tabulate,
-        gtype=GraphType.bar, baseline=None, y_as_percent=False):
+        gtype=GraphType.bar, baseline=None, y_as_percent=False, add_average=False):
     """
     Wraps the matplotlib call to the bar() / boxplot() method.
 
@@ -79,6 +81,7 @@ def draw (
     xticklabels = []
     rows = defaultdict(list)
     # for each family of benchmarks
+    worst_and_best_case = {}
     ###########################################################################
     for i,family in enumerate(families):
         # for each benchmark in the current family
@@ -133,7 +136,25 @@ def draw (
                     low_value = y - err_low
                     max_y = high_value if max_y is None else max(high_value, max_y)
                     min_y = low_value if min_y is None else min(low_value, min_y)
-                    print(bench, conf, metric, y, "low=" + str(low_value), "high=" + str(high_value))
+                    conf_str = " ".join(conf)
+                    print(bench, conf_str, metric, y, "low=" + str(low_value), "high=" + str(high_value))
+                    # TODO: use pandas?
+                    if worst_and_best_case.get(conf_str, None) is None:
+                        worst_and_best_case[conf_str] = {}
+                    if worst_and_best_case[conf_str].get(metric, None) is None:
+                        worst_and_best_case[conf_str][metric] = {
+                            "high": {"benchmark": bench, "value": y},
+                            "low": {"benchmark": bench, "value": y},
+                            "values": [y],
+                        }
+                    else:
+                        cur_max = worst_and_best_case[conf_str][metric]
+                        cur_max["values"].append(y)
+                        if cur_max["high"]["value"] < y:
+                            cur_max["high"] = {"benchmark": bench, "value": y}
+                        if cur_max["low"]["value"] > y:
+                            cur_max["low"] = {"benchmark": bench, "value": y}
+
                     #pos += widths.elements/2.0
                     # matplotlib expects [[err_low1, err_low2], [err_hi1, err_hi2]]
                     ax.bar(pos, y, yerr=[[err_low], [err_high]],
@@ -192,6 +213,24 @@ def draw (
                 for row in rows.values():
                     row.append('')
 
+    print("Worst and best cases:")
+    for wabc_conf in worst_and_best_case:
+        print("Configuration:", wabc_conf)
+        by_conf = worst_and_best_case[wabc_conf]
+        for wabc_metric in by_conf:
+            print("  Metric:", wabc_metric)
+            by_metric = by_conf[wabc_metric]
+            assert isinstance(by_metric, dict)
+            values = by_metric["values"]
+            assert isinstance(values, list)
+            by_metric["geomean"] = gmean(values)
+            by_metric["average"] = np.average(values)
+            by_metric["median"] = np.median(values)
+            for k, v in by_metric.items():
+                print("    ", k, ": ", v, sep="")
+
+    # pprint.pprint(worst_and_best_case)
+    print("--------")
     ###########################################################################
     # y axis handling
     if gtype == GraphType.bar:
